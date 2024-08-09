5243164b72379a753f86298e574b9e58f0ecfed1
Gyro <65970352+gyro2121@users.noreply.github.com>
2024-08-09 03:05:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -128,7 +128,7 @@ client.on('interactionCreate', async interaction => {
             .addFields(
                 { name: ':hourglass: Bot Latency', value: `\`\`\`${botLatency}ms\`\`\``, inline: true },
                 { name: ':hourglass: API Latency', value: `\`\`\`${apiLatency}ms\`\`\``, inline: true },
-                { name: ':bar_chart: Status', value: `${apiLatency < 100 ? '🟢 Excellent' : apiLatency < 200 ? '🟡 Good' : '🔴 Poor'}`, inline: true }
+                { name: ':bar_chart: Status', value: `\n${apiLatency < 100 ? ':green_circle: Excellent' : apiLatency < 200 ? ':yellow_circle: Good' : ':red_circle: Poor'}`, inline: true }
             )
             .setFooter({ text: 'Powered by Gyro Codes' })
             .setTimestamp();
